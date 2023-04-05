db9351b3f93391f33677f27eb07173b978ee2c14
akman12914 <rnlduadns98@naver.com>
2023-04-05 23:49:47 +0900

    JPA

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -24,6 +24,7 @@ public class User {
     private LocalDateTime updatedAt;
     private String updatedBy;
     private String plus;
+    private String check;
 
     //인수가 없는 entity생성자를 필요로 하는 이유-https://stackoverflow.com/questions/2808747/why-does-jpa-require-a-no-arg-constructor-for-domain-objects
 }
